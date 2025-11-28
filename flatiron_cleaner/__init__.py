5432e6031c4489f5749a0b27879206a72837b605
Xavier Orcutt <84699274+xavier-orcutt@users.noreply.github.com>
2025-11-27 23:34:46 -0500

    bumped up version

--- flatiron_cleaner/__init__.py
+++ flatiron_cleaner/__init__.py
@@ -4,9 +4,10 @@ FlatironCleaner
 A Python package for cleaning and harmonizing Flatiron Health cancer data.
 """
 
-__version__ = '0.1.10'
+__version__ = '0.1.11'
 
 # Make key classes available at package level
+from .general import DataProcessorGeneral
 from .urothelial import DataProcessorUrothelial
 from .nsclc import DataProcessorNSCLC
 from .colorectal import DataProcessorColorectal
@@ -14,18 +15,19 @@ from .breast import DataProcessorBreast
 from .prostate import DataProcessorProstate
 from .renal import DataProcessorRenal
 from .melanoma import DataProcessorMelanoma
-from .general import DataProcessorGeneral
+from .headneck import DataProcessorHeadNeck
 from .merge_utils import merge_dataframes
 
 # Define what gets imported with `from flatiron_cleaner import *`
 __all__ = [
+    'DataProcessorGeneral',
     'DataProcessorUrothelial',
     'DataProcessorNSCLC',
     'DataProcessorColorectal',
     'DataProcessorBreast',
     'DataProcessorProstate',
     'DataProcessorRenal',
-    'DataProcessoreMelanoma',
-    'DataProcessorGeneral',
-    'merge_dataframes',
+    'DataProcessorMelanoma',
+    'DataProcessorHeadNeck'
+    'merge_dataframes'
 ]
